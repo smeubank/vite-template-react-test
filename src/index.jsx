595c463a9c0d6109919f79c28b7acb3348b17d2d
Steven Eubank <eubank.steven88@gmail.com>
2023-07-17 19:38:19 -0500

    add sentry init
    only adding sentry nothing for source maps with vite or sentry-cli

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import * as Sentry from "@sentry/react";
+import App from "./App";
+
+Sentry.init({
+  dsn: "https://a1628e36b91245bb866ba301e9ad2699@o673219.ingest.sentry.io/4505424785178624",
+  debug: true,
+  integrations: [
+    new Sentry.BrowserTracing({
+      // See docs for support of different versions of variation of react router
+      // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+      routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+        React.useEffect,
+        //useLocation,
+        //useNavigationType,
+        //createRoutesFromChildren,
+        //matchRoutes
+      ),
+    }),
+    new Sentry.Replay()
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  tracesSampleRate: 1.0,
+
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
