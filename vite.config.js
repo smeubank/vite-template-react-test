ba03ff41772d0f3b8e9e1f37deba313161520245
Steven Eubank <eubank.steven88@gmail.com>
2023-07-17 20:59:43 -0500

    sentry vite config
    add setup to upload sourcemaps with sentry vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    sourcemap: true, // Source map generation must be turned on
+  },
+  plugins: [
+    react(),
+    // Put the Sentry vite plugin after all other plugins
+    sentryVitePlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: "steven-eubank",
+      project: "javascript-react",
+    }),
+  ]
 });
